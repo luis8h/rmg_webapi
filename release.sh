7dd3f6f461c4a8f05eb137321a17103bf0ede715
luis8h <vzbls@t-online.de>
2024-03-03 20:40:47 +0100

    release: v0.0.1

--- release.sh
+++ release.sh
@@ -6,9 +6,21 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
+# Check if the current branch is main
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [ "$CURRENT_BRANCH" != "main" ]; then
+    echo "Error: This script can only be run on the main branch."
+    exit 1
+fi
+
 VERSION="v$1"
 REGISTRY="192.168.188.179:5000"
 IMAGE_NAME="luis8h/rmg_webapi"
+
+git add .
+git commit -m "release: ${VERSION}"
+git push
+git tag $VERSION
 
 docker login "$REGISTRY"
 docker buildx build -t "$REGISTRY/$IMAGE_NAME:$VERSION" ./webapi
