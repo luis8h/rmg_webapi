0011f0dd7c979139edb236533794d7bb0f82f076
Luis Schmidmeister <vzbls@t-online.de>
2024-03-10 08:39:09 +0100

    moved staging to server

--- stage.sh
+++ stage.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-docker buildx build -t luis8h/rmg_webapi:sta ./
+docker login 192.168.188.179:5000
+docker buildx build -t 192.168.188.179:5000/luis8h/rmg_webapi:sta ./
+docker push 192.168.188.179:5000/luis8h/rmg_webapi:sta
 
