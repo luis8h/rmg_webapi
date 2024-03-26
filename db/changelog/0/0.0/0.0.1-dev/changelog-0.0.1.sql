252d3c270a1662223c3f98dc2a5ac85639d861a2
Luis Schmidmeister <vzbls@t-online.de>
2024-03-26 11:07:01 +0100

    fixed changeset for db seeding (important: rollback to v0.0.0 before)

--- db/changelog/0/0.0/0.0.1-dev/changelog-0.0.1.sql
+++ db/changelog/0/0.0/0.0.1-dev/changelog-0.0.1.sql
@@ -1,53 +1,53 @@
 -- liquibase formatted sql
 
 -- changeset luis8h:0.0.1:1
-insert into users (id, username, firstname, lastname, password, email)
+insert into users (username, firstname, lastname, password, email)
 values
-    (1, 'luis8h', 'Luis', 'Schmidmeister', 'test1', 'vzbls@t-online.de'),
-    (2, 'timo9h', 'Timo', 'Schmidmeister', 'test2', 'vzbts@t-online.de'),
-    (3, 'luis8h', 'Noah', 'Schmidmeister', 'test3', 'vzbns@t-online.de'),
-    (4, 'Wolfgang', 'Wolfgang', 'Schmidmeister', 'test4', 'vzbws@t-online.de'),
-    (5, 'ulli', 'Ulrike', 'Schmidmeister', 'test5', 'vzbus@t-online.de');
+    ('luis8h', 'Luis', 'Schmidmeister', 'test1', 'vzbls@t-online.de'),
+    ('timo9h', 'Timo', 'Schmidmeister', 'test2', 'vzbts@t-online.de'),
+    ('luis8h', 'Noah', 'Schmidmeister', 'test3', 'vzbns@t-online.de'),
+    ('Wolfgang', 'Wolfgang', 'Schmidmeister', 'test4', 'vzbws@t-online.de'),
+    ('ulli', 'Ulrike', 'Schmidmeister', 'test5', 'vzbus@t-online.de');
 
 -- rollback  truncate users cascade;
 
 
 -- changeset luis8h:0.0.1:2
-insert into tags (id, name)
+insert into tags (name)
 values
-    (1, 'Vegan'),
-    (2, 'Vegetarisch'),
-    (3, 'Süß'),
-    (4, 'Hauptspeiße'),
-    (5, 'Dip'),
-    (6, 'Suppe'),
-    (7, 'Desert'),
-    (8, 'Beilage'),
-    (9, 'Grillen'),
-    (10, 'Fleisch'),
-    (11, 'Salat'),
-    (12, 'zum Mitnehmen'),
-    (13, 'Weihnachten'),
-    (14, 'Plätzchen'),
-    (15, 'Kuchen'),
-    (16, 'Herd'),
-    (17, 'Mikrowelle'),
-    (18, 'Ofen'),
-    (19, 'Soße'),
-    (20, 'Thermomix');
+    ('Vegan'),
+    ('Vegetarisch'),
+    ('Süß'),
+    ('Hauptspeiße'),
+    ('Dip'),
+    ('Suppe'),
+    ('Desert'),
+    ('Beilage'),
+    ('Grillen'),
+    ('Fleisch'),
+    ('Salat'),
+    ('zum Mitnehmen'),
+    ('Weihnachten'),
+    ('Plätzchen'),
+    ('Kuchen'),
+    ('Herd'),
+    ('Mikrowelle'),
+    ('Ofen'),
+    ('Soße'),
+    ('Thermomix');
 
 -- rollback truncate tags cascade;
 
 
 -- changeset luis8h:0.0.1:3
-insert into recipes (id, name, description, preptime, cooktime, worktime, difficulty, created_by)
+insert into recipes (name, description, preptime, cooktime, worktime, difficulty, created_by)
 values
-    (1, 'Gewürzkuchen', '', 4, 60, 20, 2, 1),
-    (2, 'Tomatensoße', 'In erster Linie für Spaghetti, kann aber mit allen Nudelvarianten genossen werden', 0, 25, 15, 1, 1),
-    (3, 'Nudelauflauf (Tomaten/Mozarella)', '', 0, 40, 30, 2, 2),
-    (4, 'Schweinebraten', '', null, null, null, 4, 3),
-    (5, 'Germknödel', 'Kann gegebenfalls auch ohne Füllung gemacht werden.', null, null, null, 4, 3),
-    (6, 'leer', 'Dieses Rezept hat keine Tags, Bewertungen, Zeiten etc.', null, null, null, null, 1);
+    ('Gewürzkuchen', '', 4, 60, 20, 2, 1),
+    ('Tomatensoße', 'In erster Linie für Spaghetti, kann aber mit allen Nudelvarianten genossen werden', 0, 25, 15, 1, 1),
+    ('Nudelauflauf (Tomaten/Mozarella)', '', 0, 40, 30, 2, 2),
+    ('Schweinebraten', '', null, null, null, 4, 3),
+    ('Germknödel', 'Kann gegebenfalls auch ohne Füllung gemacht werden.', null, null, null, 4, 3),
+    ('leer', 'Dieses Rezept hat keine Tags, Bewertungen, Zeiten etc.', null, null, null, null, 1);
 
 -- rollback truncate recipes cascade;
 
