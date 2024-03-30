463a2d026d74dfa9730d6f1d7ae978d8d94cbc12
Luis Schmidmeister <vzbls@t-online.de>
2024-03-30 14:30:30 +0100

    tmp

--- db/changelog/0/0.0/0.0.4/changelog-0.0.4.sql
+++ db/changelog/0/0.0/0.0.4/changelog-0.0.4.sql
@@ -2,9 +2,18 @@
 
 
 -- changeset luis8h:0.0.4:1
-alter table recipe_tags drop constraint recipe_tags_tag_fkey
+alter table recipe_tags drop constraint recipe_tags_tag_fkey;
 
 --rollback changeSetId:0.0.0:11 changeSetAuthor:luis8h changeSetPath:changelog/0/0.0/0.0.0/changelog-0.0.0.sql
+
+
+-- changeset luis8h:0.0.4:2
+alter table recipe_tags
+add constraint recipe_tags_tag_fkey
+    foreign key (tag) references tags(id)
+    on delete cascade;
+
+-- rollback alter table recipe_tags drop constraint recipe_tags_tag_fkey
 
 
 
