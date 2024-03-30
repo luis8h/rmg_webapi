dde6431be036a16c5c8fd6cc274f89c7de62eccf
luis8h <vzbls@t-online.de>
2024-03-30 13:24:08 +0100

    tmp

--- db/changelog/0/0.0/0.0.4/changelog-0.0.4.sql
+++ db/changelog/0/0.0/0.0.4/changelog-0.0.4.sql
@@ -4,7 +4,7 @@
 -- changeset luis8h:0.0.4:1
 alter table recipe_tags drop constraint recipe_tags_tag_fkey
 
---rollback luis8h:0.0.0:11 changeSetPath:folder1/db-changelog1.sql
+--rollback changeSetId:0.0.0:11 changeSetAuthor:luis8h changeSetPath:changelog/0/0.0/0.0.0/changelog-0.0.0.sql
 
 
 
