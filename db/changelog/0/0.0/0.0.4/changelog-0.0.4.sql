4480318b6a9c8c0cc5d7d1d0535f001041bc5955
Luis Schmidmeister <vzbls@t-online.de>
2024-03-30 14:42:09 +0100

    tmp

--- db/changelog/0/0.0/0.0.4/changelog-0.0.4.sql
+++ db/changelog/0/0.0/0.0.4/changelog-0.0.4.sql
@@ -3,7 +3,6 @@
 
 -- changeset luis8h:0.0.4:1
 alter table recipe_tags drop constraint recipe_tags_tag_fkey;
-
 --rollback changeSetId:0.0.0:11 changeSetAuthor:luis8h changeSetPath:changelog/0/0.0/0.0.0/changelog-0.0.0.sql
 
 
@@ -12,8 +11,32 @@ alter table recipe_tags
 add constraint recipe_tags_tag_fkey
     foreign key (tag) references tags(id)
     on delete cascade;
-
 -- rollback alter table recipe_tags drop constraint recipe_tags_tag_fkey
 
+
+-- changeset luis8h:0.0.4:3
+alter table recipe_tags drop constraint recipe_tags_recipe_fkey;
+--rollback changeSetId:0.0.0:10 changeSetAuthor:luis8h changeSetPath:changelog/0/0.0/0.0.0/changelog-0.0.0.sql
+
+
+-- changeset luis8h:0.0.4:4
+alter table recipe_tags
+add constraint recipe_tags_recipe_fkey
+    foreign key (recipe) references recipe(id)
+    on delete cascade;
+-- rollback alter table recipe_tags drop constraint recipe_tags_recipe_fkey
+
+
+-- changeset luis8h:0.0.4:5
+alter table ratings drop constraint fk_recipe;
+--rollback changeSetId:0.0.0:8 changeSetAuthor:luis8h changeSetPath:changelog/0/0.0/0.0.0/changelog-0.0.0.sql
+
+
+-- changeset luis8h:0.0.4:6
+alter table ratings
+add constraint ratings_recipe_fkey
+    foreign key (recipe) references recipe(id)
+    on delete cascade;
+-- rollback alter table recipe_tags drop constraint recipe_tags_recipe_fkey
 
 
