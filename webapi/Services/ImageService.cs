523794fdc5082eef539d1f416c4975d781dacf3f
Luis Schmidmeister <vzbls@t-online.de>
2024-05-10 16:40:59 +0200

    removed test directory

--- webapi/Services/ImageService.cs
+++ webapi/Services/ImageService.cs
@@ -6,17 +6,10 @@ namespace webapi.Services
 {
     public class ImageService
     {
-        public async Task CompressAndSaveImageAsync(IFormFile imageFile, string outputPath, int quality = 75)
+        public async Task CompressAndSaveImageAsync(IFormFile imageFile, string outputPath)
         {
             using var imageStream = imageFile.OpenReadStream();
             using var image = Image.Load(imageStream);
-
-            // var encoder = new JpegEncoder
-            // {
-            //     Quality = quality,
-            //             // CompressionLevel = PngCompressionLevel.BestCompression,
-            //             // BitDepth = PngBitDepth.Bit1,
-            // };
 
             var encoder = new WebpEncoder();
 
